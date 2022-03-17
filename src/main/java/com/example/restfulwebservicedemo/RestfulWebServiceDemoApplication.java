2873d5f59aa2f1278c6ad15096020b596b3b7c39
dayan <eam.dayan>
2022-03-17 07:57:37 +0700

    first commit

---
+++ src/main/java/com/example/restfulwebservicedemo/RestfulWebServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulwebservicedemo/RestfulWebServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
